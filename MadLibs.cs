8630617ef73e463d9b4a39d4e6f3dbfa11e4d0d5
Jason Bagwill <mr.bagwill@gmail.com>
2022-05-19 20:05:22 -0400

    added madlibs

--- MadLibs.cs
+++ MadLibs.cs
@@ -7,28 +7,50 @@ namespace MadLibs
     static void Main(string[] args)
     {
       /*
-      This program is a MadLib designed for unique inputs
-      Author: ...Jason Not sure why this isn't working
-      */
-
-
-      // Let the user know that the program is starting:
-
-
-      // Give the Mad Lib a title:
-      string title = "TITLE";
-
+      This program is a MadLib to practice inputs
+      Author:Jason Bagwill
+      */      
+      Console.WriteLine("Mad Libs is starting!");
+      
+      string title = "Today is going to be ...";
       Console.WriteLine(title);
-      // Define user input and variables:
 
+      
+      Console.Write("Enter a name: ");
+      string name = Console.ReadLine();
+      Console.Write("Enter an adjective: ");
+      string adjective = Console.ReadLine();
+      Console.Write("Enter an adjective: ");
+      string adjective2 = Console.ReadLine();      
+      Console.Write("Enter an adjective: ");
+      string adjective3 = Console.ReadLine();
+      Console.Write("Enter an verb: ");
+      string verb = Console.ReadLine();
+      Console.Write("Enter an noun: ");
+      string noun1 = Console.ReadLine();      
+      Console.Write("Enter an noun: ");
+      string noun2 = Console.ReadLine();
+      Console.Write("Enter an animal: ");
+      string animal = Console.ReadLine();      
+      Console.Write("Enter a food: ");
+      string food = Console.ReadLine();
+      Console.Write("Enter a fruit: ");
+      string fruit = Console.ReadLine();      
+      Console.Write("Enter a superhero: ");
+      string superhero = Console.ReadLine();
+      Console.Write("Enter a country: ");
+      string country = Console.ReadLine();      
+      Console.Write("Enter a dessert: ");
+      string dessert = Console.ReadLine();
+      Console.Write("Enter a year: ");
+      string year = Console.ReadLine();
 
-      // The template for the story:
+      
+      string story = $"This morning {name} woke up feeling {adjective}. 'It is going to be a {adjective2} day!' Outside, a bunch of {animal}s were protesting to keep {food}s in stores. They began to {verb} to the rhythm of the {noun1}, which made all the {fruit}s very {adjective3}. Concerned, {name} texted {superhero}, who flew {name} to {country} and dropped {name} in a puddle of frozen {dessert}. {name} woke up in the year {year}, in a world where {noun2}s ruled the world.";
 
-      string story = "This morning _ woke up feeling _. 'It is going to be a _ day!' Outside, a bunch of _s were protesting to keep _ in stores. They began to _ to the rhythm of the _, which made all the _s very _. Concerned, _ texted _, who flew _ to _ and dropped _ in a puddle of frozen _. _ woke up in the year _, in a world where _s ruled the world.";
-
-
-      // Print the story:
-
+      
+      Console.WriteLine(story);
     }
   }
 }
+
