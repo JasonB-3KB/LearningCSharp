e7ffd545078bd4b527987d8bfebc1e90ef2d021d
Jason Bagwill <mr.bagwill@gmail.com>
2022-05-26 17:51:30 -0400

    Pick

---
+++ PickANumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 7;
+            Console.Write("Guess a number between 1 and 10: ");
+            string guess = Console.ReadLine();
+            int guessInt = Int32.Parse(guess);
+            if (guessInt > number)
+            {
+                Console.WriteLine("You guessed too high");
+            }
+            else if (guessInt < number)
+            {
+                Console.WriteLine("You guessed too low");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!!!");
+            }
+
+        }
+    }
+}
+
